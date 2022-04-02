402d9871679dff2d0601b3f38fec625dd6e598dc
Michael-August <michaelfrancis135@gmail.com>
2022-04-02 16:35:14 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import AddTask from "./components/AddTask";
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 function App() {
+  const [ tasks, setTask ] = useState([])
+  const [showAddForm, setShowAddForm] = useState(false)
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      const res = await fetch('http://localhost:4000/tasks')
+      const data = await res.json
+      console.log(data);
+    }
+    fetchTasks()
+  }, [])
+
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task }
+    setTask([...tasks, newTask])
+  }
+  
+  let deleteTask = (id) => {
+    setTask(tasks.filter((task) => task.id !== id))
+  }
+
+  // Toggle reminder
+  const toggleReminder = (id) => {
+    setTask(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+  }
+
+  const toggleshowAddForm = () => {
+    setShowAddForm(!showAddForm)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header onAdd = {toggleshowAddForm} showAdd={showAddForm} />
+      {showAddForm && <AddTask onAddTask={ addTask } />}
+      {tasks.length > 0 ?<Tasks tasks={ tasks } ondelete={deleteTask} ondoubleclick={toggleReminder} /> : "No task to show"}
     </div>
   );
 }
